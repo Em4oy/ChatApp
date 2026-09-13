d0ae2be08f92a34f18a0abe4b7be13c535f8ec66
Em4oy <eyordanovv@gmail.com>
2026-09-13 20:38:33 +0300

    chat app 4
    chat app

--- mail_test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$success = mail('1@example.com', 'My Subject', "What time is it?");
-if (!$success) {
-    $errorMessage = error_get_last()['message'];
-	echo $errorMessage . " - error";
-}
-?>
